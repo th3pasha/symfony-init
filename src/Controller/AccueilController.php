930df45043c60f735891d03725ebc1d5384f0d07
Ayoub <elpasha117@gmail.com>
2023-11-13 22:56:31 +0100

    impl Resto entity

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Controller;
+use App\Entity\Resto;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -8,7 +9,28 @@ class AccueilController extends AbstractController{
     function afficher($name) {
         return $this->render('/accueil/accueil.html.twig', array('name' => $name));
     }
+
+    function voir($id){
+        $resto = $this->getDoctrine()->getRepository(Resto::class)->find($id);
+        if (!$resto){
+            throw $this->createNotFoundException('Salle[id='.$id.') inexsitant');
+        }
+        return $this->render('/accueil/voir.html.twig', array('resto' => $resto));
+    }
+
+    function ajouter($nom, $chef, $nbreEtoile){
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $resto = new Resto();
+        $resto->setNom($nom);
+        $resto->setChef($chef);
+        $resto->setNbreEtoile($nbreEtoile);
+
+        $entityManager->persist($resto);
+        $entityManager->flush();
+
+        return $this->redirect('guide_michelin_voir', array('id' => $resto->getId()));
+
+    }
 }
-
-
 ?>
